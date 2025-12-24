f151108db67103221ca4a6521cf2638cc8f337cf
joao <joao.simsic@4x4brasil.com.br>
2025-12-23 21:10:22 -0300

    feat: create order use case

---
+++ src/application/use-cases/order/CreateOrderUseCase.ts
@@ -0,0 +1,31 @@
+import { Order } from "../../../domain/entities/Order";
+import { IOrderRepository } from "../../../domain/repositories/IOrderRepository";
+import { Service } from "../../../domain/entities/Service";
+
+interface CreateOrderInput {
+	lab: string;
+	patient: string;
+	customer: string;
+	services: { name: string; value: number }[];
+}
+
+export class CreateOrderUseCase {
+	constructor(private orderRepository: IOrderRepository) { }
+
+	async execute(input: CreateOrderInput): Promise<void> {
+		const services = input.services.map(
+			(s) => new Service(s.name, s.value, "PENDING"),
+		);
+
+		const order = new Order(
+			input.lab,
+			input.patient,
+			input.customer,
+			"CREATED",
+			"ACTIVE",
+			services,
+		);
+
+		await this.orderRepository.save(order);
+	}
+}
